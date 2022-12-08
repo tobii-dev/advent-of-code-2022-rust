3a7f8193fac50210f4bc267e1bc03534ad874b60
tobii-dev <13118400-tobii-dev@users.noreply.gitlab.com>
2022-12-08 02:34:01 +0100

    day-07 overengineering

--- day-07/src/lib.rs
+++ day-07/src/lib.rs
@@ -1,8 +1,87 @@
 use std::cell::RefCell;
 use std::rc::Rc;
 
-use std::collections::BTreeSet;
 
+
+#[derive(Debug)]
+enum DeviceError {
+    DirAlreadyExists(DirError),
+    VarAlreadyExists,
+    DirNotFound,
+    DirAboveRoot,
+}
+
+
+struct Device {
+    root: Rc<RefCell<Dir>>,
+    ptr: Rc<RefCell<Dir>>,
+}
+
+
+impl Device {
+    fn new() -> Self {
+        let root = Rc::new(RefCell::new(Dir::new()));
+        let ptr = Rc::clone(&root);
+        Self { root, ptr }
+    }
+
+    fn add_dir(&mut self, name: &str) -> Result<(), DeviceError> {
+        let dir = self
+            .ptr
+            .borrow_mut()
+            .add_dir(name)
+            .map_err(|e| DeviceError::DirAlreadyExists(e))?;
+        dir.borrow_mut().root = Some(self.ptr.clone());
+        Ok(())
+    }
+
+    fn add_var(&mut self, name: &str, size: usize) -> Result<(), DeviceError> {
+        self.ptr
+            .borrow_mut()
+            .add_var(name, size)
+            .map_err(|_| DeviceError::VarAlreadyExists)?;
+        Ok(())
+    }
+
+    fn cd(&mut self, name: Option<&str>) -> Result<(), DeviceError> {
+        let dir: Rc<RefCell<Dir>> = match name {
+            Some(name) => {
+                match self
+                    .ptr
+                    .borrow()
+                    .dirs
+                    .iter()
+                    .find(|v| v.borrow().name == name)
+                {
+                    Some(r) => r.clone(),
+                    None => return Err(DeviceError::DirNotFound),
+                }
+            }
+            None => match &self.ptr.borrow().root {
+                Some(r) => r.clone(),
+                None => return Err(DeviceError::DirAboveRoot),
+            },
+        };
+        self.ptr = dir;
+        Ok(())
+    }
+
+    fn get_all_dirs(&self) -> Vec<Rc<RefCell<Dir>>> {
+        return self.root.borrow().get_sub_dirs();
+    }
+
+    fn get_total_size(&self) -> usize {
+        self.root.borrow_mut().calc_size();
+        self.root.borrow().size
+    }
+}
+
+
+#[derive(Debug)]
+enum DirError {
+    DirAlreadyExists,
+    VarAlreadyExists,
+}
 
 
 struct Dir {
@@ -12,6 +91,7 @@ struct Dir {
     dirs: Vec<Rc<RefCell<Dir>>>,
     vars: Vec<(String, usize)>,
 }
+
 
 impl Dir {
     fn new() -> Self {
@@ -23,203 +103,144 @@ impl Dir {
             vars: vec![],
         }
     }
-}
 
-impl std::fmt::Display for Dir {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let lvl = f.width().unwrap_or(0);
-        let prefix = "\t".repeat(lvl);
-        let name = &self.name;
-        let size = &self.size;
-        writeln!(f, "{prefix}\"{name}\" # ({size}) [")?;
-
-        let len_dirs = self.dirs.len();
-        writeln!(f, "{prefix}\tdirs: ~ {len_dirs} (")?;
-        for dir in &self.dirs {
-            let dir = dir.borrow();
-            let l = lvl + 1;
-            writeln!(f, "{dir:l$}")?;
+    fn add_dir(&mut self, name: &str) -> Result<Rc<RefCell<Self>>, DirError> {
+        if self.dirs.iter().find(|v| v.borrow().name == name).is_some() {
+            return Err(DirError::DirAlreadyExists);
         }
-        writeln!(f, "\t{prefix})")?;
+        let dir = Dir {
+            name: name.to_string(),
+            size: 0,
+            root: None, //TODO!
+            dirs: vec![],
+            vars: vec![],
+        };
+        let dir = Rc::new(RefCell::new(dir));
+        self.dirs.push(dir.clone());
+        Ok(dir)
+    }
 
-        let len_vars = self.vars.len();
-        writeln!(f, "{prefix}\tvars: ~ {len_vars} {{")?;
-        for var in &self.vars {
-            let (var_name, var_size) = var;
-            writeln!(f, "{prefix}\t\t{var_name}: ({var_size})")?;
+    fn add_var(&mut self, name: &str, size: usize) -> Result<(), DirError> {
+        if self
+            .vars
+            .iter()
+            .find(|(var_name, _var_size)| var_name == name)
+            .is_some()
+        {
+            return Err(DirError::VarAlreadyExists);
         }
-        writeln!(f, "{prefix}\t}}")?;
-
-        writeln!(f, "{prefix}]")?;
-
+        self.vars.push((name.to_string(), size));
         Ok(())
+    }
+
+    fn get_sub_dirs(&self) -> Vec<Rc<RefCell<Dir>>> {
+        let mut v: Vec<Rc<RefCell<Dir>>> = vec![];
+        for dir in self.dirs.iter() {
+            for sub_dir in dir.borrow().get_sub_dirs() {
+                v.push(sub_dir.clone());
+            }
+            v.push(dir.clone());
+        }
+        v
+    }
+
+    fn calc_size(&mut self) {
+        self.size = 0;
+        for (_name, size) in self.vars.iter() {
+            self.size += size;
+        }
+        for dir in self.dirs.iter() {
+            dir.borrow_mut().calc_size();
+            self.size += dir.borrow().size;
+        }
     }
 }
 
 pub fn p1(lines: &Vec<String>) -> usize {
-    let mut total = 0;
     const MAX_SIZE: usize = 100_000;
-    let root = Rc::new(RefCell::new(Dir::new()));
-    let mut ptr = Rc::clone(&root);
+    let mut dev = Device::new();
 
     for line in lines {
         if line.starts_with("$ cd /") {
-            //?
+            //
         } else if line.starts_with("$ ls") {
-            //?
+            //
         } else if line.starts_with("dir ") {
             let name = line.split_whitespace().last().unwrap();
-            assert!(&ptr
-                .borrow()
-                .dirs
-                .iter()
-                .find(|v| v.borrow().name == name)
-                .is_none());
-            let dir = Dir {
-                name: name.to_string(),
-                size: 0,
-                root: None,
-                dirs: vec![],
-                vars: vec![],
-            };
-            let dir = Rc::new(RefCell::new(dir));
-            ptr.borrow_mut().dirs.push(Rc::clone(&dir));
-            dir.borrow_mut().root = Some(Rc::clone(&ptr));
+            dev.add_dir(&name).unwrap();
         } else if line.starts_with("$ cd ..") {
-            let size = ptr.borrow().size;
-            if size <= MAX_SIZE {
-                total += size;
-            }
-            let ptr_clone = Rc::clone(&ptr);
-            ptr = Rc::clone(ptr_clone.borrow().root.as_ref().unwrap());
-            ptr.borrow_mut().size += size;
+            dev.cd(None).unwrap();
         } else if line.starts_with("$ cd ") {
             let name: String = line.split_whitespace().last().unwrap().to_string();
-
-            let ptr_clone = Rc::clone(&ptr);
-            let dir = Rc::clone(
-                ptr_clone
-                    .borrow()
-                    .dirs
-                    .iter()
-                    .find(|v| v.borrow().name == name)
-                    .unwrap(),
-            );
-            assert!(dir.borrow().size == 0);
-            ptr = Rc::clone(&dir);
+            dev.cd(Some(&name)).unwrap();
         } else if line.chars().next().unwrap().is_numeric() {
             let mut words = line.split_whitespace();
             let size: usize = words.next().unwrap().parse().unwrap();
             let name: String = words.next().unwrap().to_string();
-            assert!(&ptr
-                .borrow()
-                .vars
-                .iter()
-                .find(|(v, _size)| v == &name)
-                .is_none());
-            {
-                let mut ptr = ptr.borrow_mut();
-                ptr.size += size;
-                ptr.vars.push((name, size));
-            }
+            dev.add_var(&name, size).unwrap();
         }
     }
-    let rt = root.borrow();
-    println!("{rt}");
-    total
+    dev.get_total_size();
+    dev.get_all_dirs()
+        .iter()
+        .filter_map(|v| {
+            let size = v.borrow().size;
+            if size <= MAX_SIZE {
+                return Some(size);
+            };
+            return None;
+        })
+        .sum()
 }
 
 pub fn p2(lines: &Vec<String>) -> usize {
     const MAX_SIZE: usize = 70_000_000;
     const REQ_SIZE: usize = 30_000_000;
     const TOP_SIZE: usize = MAX_SIZE - REQ_SIZE;
-    let mut canditates = BTreeSet::new();
 
-    let root = Rc::new(RefCell::new(Dir::new()));
-    let mut ptr = Rc::clone(&root);
+
+    let mut dev = Device::new();
 
     for line in lines {
         if line.starts_with("$ cd /") {
+            //?
         } else if line.starts_with("$ ls") {
-
+            //?
         } else if line.starts_with("dir ") {
             let name = line.split_whitespace().last().unwrap();
-            assert!(&ptr
-                .borrow()
-                .dirs
-                .iter()
-                .find(|v| v.borrow().name == name)
-                .is_none());
-            let dir = Dir {
-                name: name.to_string(),
-                size: 0,
-                root: None,
-                dirs: vec![],
-                vars: vec![],
-            };
-            let dir = Rc::new(RefCell::new(dir));
-            ptr.borrow_mut().dirs.push(Rc::clone(&dir));
-            dir.borrow_mut().root = Some(Rc::clone(&ptr));
-
+            dev.add_dir(&name).unwrap();
         } else if line.starts_with("$ cd ..") {
-            let size = ptr.borrow().size;
-            let ptr_clone = Rc::clone(&ptr);
-            ptr = Rc::clone(ptr_clone.borrow().root.as_ref().unwrap());
-            ptr.borrow_mut().size += size;
-            canditates.insert(size); // TODO: check if insert can be ommited based on prev info.
-
+            dev.cd(None).unwrap();
         } else if line.starts_with("$ cd ") {
             let name: String = line.split_whitespace().last().unwrap().to_string();
-
-            let ptr_clone = Rc::clone(&ptr); //TODO: Is this needed?
-            let dir = Rc::clone(
-                ptr_clone
-                    .borrow()
-                    .dirs
-                    .iter()
-                    .find(|v| v.borrow().name == name)
-                    .unwrap(),
-            );
-            assert!(dir.borrow().size == 0);
-            ptr = Rc::clone(&dir);
+            dev.cd(Some(&name)).unwrap();
         } else if line.chars().next().unwrap().is_numeric() {
             let mut words = line.split_whitespace();
             let size: usize = words.next().unwrap().parse().unwrap();
             let name: String = words.next().unwrap().to_string();
-            assert!(&ptr
-                .borrow()
-                .vars
-                .iter()
-                .find(|(v, _size)| v == &name)
-                .is_none());
-            {
-                let mut ptr = ptr.borrow_mut();
-                ptr.size += size;
-                ptr.vars.push((name, size));
-            }
+            dev.add_var(&name, size).unwrap();
         }
     }
+    let total = dev.get_total_size();
+    assert!(total < MAX_SIZE);
+    assert!(total > TOP_SIZE);
 
-    while ptr.borrow().root.is_some() { // crawl back to root
-        let ptr_clone = Rc::clone(&ptr);
-        let size = ptr.borrow().size;
-        ptr = Rc::clone(ptr_clone.borrow().root.as_ref().unwrap());
-        ptr.borrow_mut().size += size;
-        canditates.insert(size);
+    let target = total - TOP_SIZE;
+
+    // let mut canditates = std::collections::BTreeSet::new();
+    // for dir in dev.get_all_dirs() {
+    //     canditates.insert(dir.borrow().size);
+    // }
+    //return (&canditates).range(target..).next().unwrap();
+
+    let mut smallest = total;
+    for dir in dev.get_all_dirs() {
+        let size = dir.borrow().size;
+        if (size >= target) && (size < smallest) {
+            smallest = size;
+        }
     }
-
-    assert!(ptr.as_ptr() == root.as_ptr()); //FIXME: sure?
-    let total_used = ptr.borrow().size;
-    dbg!(&total_used);
-    dbg!(&canditates);
-
-    assert!(total_used > TOP_SIZE);
-    let target = total_used - TOP_SIZE;
-    dbg!(&target);
-
-    let f = (&canditates).range(target..).next().unwrap();
-    dbg!(&f);
-    *f
+    smallest
 }
 
 #[cfg(test)]
