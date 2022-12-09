b7742c688f7b21e93d904ee5dfdd026c0489ebe2
tobii-dev <13118400-tobii-dev@users.noreply.gitlab.com>
2022-12-09 20:57:55 +0100

    day-09 part2

--- day-09/src/lib.rs
+++ day-09/src/lib.rs
@@ -12,6 +12,63 @@ impl std::fmt::Display for Pos {
 	}
 }
 
+impl Pos {
+	fn follow(&mut self, next: &Pos) -> bool {
+		if (next.x - 1..=next.x + 1).contains(&self.x)
+			&& (next.y - 1..=next.y + 1).contains(&self.y)
+		{
+			return false; // no need to move
+		}
+		let (dx, dy) = (next.x - self.x, next.y - self.y);
+		self.x += dx.signum();
+		self.y += dy.signum();
+		true
+	}
+
+	#[allow(unused)]
+	fn diags(&self) -> [Pos; 4] {
+		[
+			Pos {
+				x: self.x - 1,
+				y: self.y - 1,
+			}, //LU
+			Pos {
+				x: self.x + 1,
+				y: self.y - 1,
+			}, //RU
+			Pos {
+				x: self.x + 1,
+				y: self.y + 1,
+			}, //RD
+			Pos {
+				x: self.x - 1,
+				y: self.y + 1,
+			}, //LD
+		]
+	}
+	#[allow(unused)]
+	fn cross(&self) -> [Pos; 4] {
+		[
+			Pos {
+				x: self.x - 1,
+				y: self.y,
+			},
+			Pos {
+				x: self.x,
+				y: self.y - 1,
+			},
+			Pos {
+				x: self.x + 1,
+				y: self.y,
+			},
+			Pos {
+				x: self.x,
+				y: self.y + 1,
+			},
+		]
+	}
+}
+
 struct Grid {
 	x_min: isize,
 	y_min: isize,
@@ -20,7 +77,7 @@ struct Grid {
 	y_max: isize,
 
 	head: Pos,
-	tail: Pos,
+	tails: Vec<Pos>,
 	tail_visits: Vec<Pos>,
 }
 
@@ -29,26 +86,28 @@ impl std::fmt::Display for Grid {
 		for y in self.y_min..=self.y_max {
 			for x in self.x_min..=self.x_max {
 				let is_visit = self.tail_visits.iter().any(|p| (p.x == x) && (p.y == y));
-				let is_tail = (self.tail.x == x) && (self.tail.y == y);
+				let is_tail = self.tails.iter().any(|p| (p.x == x) && (p.y == y));
 				let is_head = (self.head.x == x) && (self.head.y == y);
+				let mut c = " ";
 				if is_visit {
 					write!(f, "{{")?;
+					c = "}";
 				} else {
-					write!(f, "[")?;
+					write!(f, " ")?;
 				}
 				if is_head && is_tail {
-					write!(f, "{:>2}", "X")?;
+					write!(f, "{:>2}{c}", "X")?;
 				} else if is_head {
-					write!(f, "{:>2}", "H")?;
+					write!(f, "{:>2}{c}", "H")?;
 				} else if is_tail {
-					write!(f, "{:>2}", "T")?;
+					let v = self
+						.tails
+						.iter()
+						.position(|p| p.x == x && p.y == y)
+						.unwrap();
+					write!(f, "{v:>2}{c}")?;
 				} else {
-					write!(f, "{:>2}", "")?;
-				}
-				if is_visit {
-					write!(f, "}}")?;
-				} else {
-					write!(f, "]")?;
+					write!(f, "{:>2}{c}", "·")?;
 				}
 			}
 			writeln!(f)?;
@@ -58,20 +117,20 @@ impl std::fmt::Display for Grid {
 }
 
 impl Grid {
-	fn new() -> Self {
+	fn new(c: usize) -> Self {
 		Grid {
 			x_min: 0,
 			y_min: 0,
 			x_max: 0,
 			y_max: 0,
 			head: Pos { x: 0, y: 0 },
-			tail: Pos { x: 0, y: 0 },
+			tails: vec![Pos { x: 0, y: 0 }; c],
 			tail_visits: vec![Pos { x: 0, y: 0 }],
 		}
 	}
 
-	fn from(lines: &Vec<String>) -> Self {
-		let mut grid = Grid::new();
+	fn from(lines: &Vec<String>, tails: usize) -> Self {
+		let mut grid = Grid::new(tails);
 		for line in lines {
 			let mut words = line.split_whitespace();
 			let (dir, count) = (
@@ -100,7 +159,6 @@ impl Grid {
 	fn update_head(&mut self, count: usize, dx: isize, dy: isize) -> bool {
 		let mut update = false;
 		for _ in 0..count {
-			let prev = self.head;
 			self.head.x += dx;
 			self.head.y += dy;
 			if self.head.x < self.x_min {
@@ -115,43 +173,38 @@ impl Grid {
 			if self.head.y > self.y_max {
 				self.y_max = self.head.y;
 			}
-			if self.update_tail(&prev) {
-				update = self.update_visits();
+			let mut next = self.head;
+			for tail in self.tails.iter_mut() {
+				if tail.follow(&next) {
+				}
+				next = *tail;
 			}
+			update = self.update_visits();
 		}
 		update
 	}
 
-	fn update_tail(&mut self, prev: &Pos) -> bool {
-		if (self.head.x - 1..=self.head.x + 1).contains(&self.tail.x)
-			&& (self.head.y - 1..=self.head.y + 1).contains(&self.tail.y)
-		{
-			return false;
-		}
-		self.tail.x = prev.x;
-		self.tail.y = prev.y;
-		true
-	}
-
 	fn update_visits(&mut self) -> bool {
-		if !self.tail_visits.iter().any(|p| p == &self.tail) {
-			self.tail_visits.push(Pos {
-				x: self.tail.x,
-				y: self.tail.y,
-			});
+		let tail = self.tails.last().unwrap();
+		if !self.tail_visits.iter().any(|p| p == tail) {
+			self.tail_visits.push(*tail);
+			return true;
 		}
 		false
 	}
 }
 
 pub fn p1(lines: &Vec<String>) -> usize {
-	let grid = Grid::from(lines);
+	let grid = Grid::from(lines, 1);
+    // println!("{grid}");
 	grid.tail_visits.len()
 }
 
-// pub fn p2(lines: &Vec<String>) -> usize {
-// 	todo!()
-// }
+pub fn p2(lines: &Vec<String>) -> usize {
+	let grid = Grid::from(lines, 9);
+    // println!("{grid}");
+	grid.tail_visits.len()
+}
 
 #[cfg(test)]
 mod tests {
@@ -180,25 +233,25 @@ mod tests {
 		assert_eq!(r, 5981);
 	}
 
-	// #[test]
-	// fn example2() {
-	// 	let fd = std::fs::File::open("example.txt").unwrap();
-	// 	let mut lines: Vec<String> = std::io::BufReader::new(fd)
-	// 		.lines()
-	// 		.map(|l| l.unwrap())
-	// 		.collect();
-	// 	let r = p2(&mut lines);
-	// 	assert_eq!(r, 8);
-	// }
+	#[test]
+	fn example2() {
+		let fd = std::fs::File::open("example2.txt").unwrap();
+		let mut lines: Vec<String> = std::io::BufReader::new(fd)
+			.lines()
+			.map(|l| l.unwrap())
+			.collect();
+		let r = p2(&mut lines);
+		assert_eq!(r, 36);
+	}
 
-	// #[test]
-	// fn part2() {
-	// 	let fd = std::fs::File::open("input.txt").unwrap();
-	// 	let mut lines: Vec<String> = std::io::BufReader::new(fd)
-	// 		.lines()
-	// 		.map(|l| l.unwrap())
-	// 		.collect();
-	// 	let r = p2(&mut lines);
-	// 	assert_eq!(r, 235200);
-	// }
+	#[test]
+	fn part2() {
+		let fd = std::fs::File::open("input.txt").unwrap();
+		let mut lines: Vec<String> = std::io::BufReader::new(fd)
+			.lines()
+			.map(|l| l.unwrap())
+			.collect();
+		let r = p2(&mut lines);
+		assert_eq!(r, 2352);
+	}
 }
